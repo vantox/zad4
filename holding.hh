39bc31120415b8736dc73c21fb67733e08333ee2
Vantox <mike27k@gmail.com>
2013-11-28 11:41:04 +0100

    poprawione wszystko

--- holding.hh
+++ holding.hh
@@ -368,14 +368,12 @@ multiplicative_rollup_group(Group<C> const &s1)
 
 template<class C1, class C2, class C3>
 bool solve_auction(Group<C1> const &g1, Group<C2> const &g2, Group<C3> const &g3)
-{	/*
+{	
 	bool solve = false;
 	if((g1 > g2) && (g1 > g3)) solve = true;
 	if((g2 > g1) && (g2 > g3)) solve = true;
 	if((g3 > g2) && (g3 > g1)) solve = true;
-	return solve;	
-	*/
-	return g1 > g2;
+	return solve;
 }
 
 #endif
